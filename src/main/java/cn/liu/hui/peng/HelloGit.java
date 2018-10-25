00b47c461b0477661a4c29bd45a74b0462a410d3
hz16092620 <liuhuipeng@huize.com>
2018-10-25 09:57:07 +0800

    eclipse hello to git.

---
+++ src/main/java/cn/liu/hui/peng/HelloGit.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2006-2015 Hzins Ltd. All Rights Reserved. 
+ *  
+ * This code is the confidential and proprietary information of   
+ * Hzins. You shall not disclose such Confidential Information   
+ * and shall use it only in accordance with the terms of the agreements   
+ * you entered into with Hzins,http://www.hzins.com.
+ *  
+ */   
+package cn.liu.hui.peng; 
+
+/**
+ * <p>
+ * 
+ *
+ *
+ * </p>
+ * @author	hz16092620 
+ * @date	2018年10月25日 上午9:50:21
+ * @version      
+ */
+public class HelloGit {
+    
+    public static void main(String[] args) {
+	System.out.println("hello git!!!");
+    }
+}
+ 
