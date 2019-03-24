d91d4ab8a100e392cda862f18f37271dc290fe0c
Liuhp534 <617456341@qq.com>
2019-03-24 14:15:30 +0800

    ThreadLocalUtils.

--- src/main/java/cn/liu/hui/peng/list/ForEachTest.java
+++ src/main/java/cn/liu/hui/peng/list/ForEachTest.java
@@ -1,32 +1,34 @@
-package cn.liu.hui.peng.list; 
+package cn.liu.hui.peng.list;
 
 /**
  * <p>
- * 
+ *
  *
  *
  * </p>
- * @author	hz16092620 
- * @date	2018年10月6日 上午10:20:22
- * @version      
+ *
+ * @author hz16092620
+ * @date 2018年10月6日 上午10:20:22
  */
 public class ForEachTest {
-    
+
     public static void main(String[] args) {
-	//forEach();
-	applyStyle(1 | 2);
+        //forEach();
+        applyStyle(1 | 2);
     }
 
-    /**机制是先创建一个数组，大小为参数个数，无参数则一个为0的空数组不是null。*/
-    static void forEach(int ... ints) {
-	System.out.println(ints.length);
-	for (int i : ints) {
-	    System.out.println(i);
-	}
+    /**
+     * 机制是先创建一个数组，大小为参数个数，无参数则一个为0的空数组不是null。
+     */
+    static void forEach(int... ints) {
+        System.out.println(ints.length);
+        for (int i : ints) {
+            System.out.println(i);
+        }
     }
-    
+
     static void applyStyle(int i) {
-	System.out.println(i);
+        System.out.println(i);
     }
 }
  
